4de2f99b02c2a614a7e204ce3486144d46d2e9f2
ArusyakGevorgyan <arusikgevorgyan95@gmail.com>
2021-05-01 11:41:10 +0400

    commit

---
+++ js/script.js
@@ -0,0 +1,23 @@
+window.addEventListener('load', () => {
+    const mobileMenuBtn = document.querySelector('#mobile_menu_btn');
+    const navList = document.querySelector('#nav-list');
+
+    mobileMenuBtn.addEventListener('click', () => {
+        navList.classList.toggle('menu_active');
+    })
+});
+
+
+
+var swiper = new Swiper('.swiper-container', {
+    direction: 'vertical',
+    loop: true,
+    autoplay: {
+        delay: 500,
+        disableOnInteraction: false,
+    },
+  });
+
+  
+
+  
